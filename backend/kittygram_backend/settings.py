642cd6ac5089c10ede1f9ee0ecc867930181703f
Андрей Бузинов <andre.buzinov@yandex.ru>
2024-06-15 13:04:40 +0300

    IP

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '158.160.71.7',
+    '51.250.104.53',
     '127.0.0.1',
     'localhost',
     'maufen.zapto.org'
