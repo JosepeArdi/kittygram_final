e323b251b9eeb8e0d093d2f3192e48acfc468349
Андрей Бузинов <andre.buzinov@yandex.ru>
2024-06-16 20:11:05 +0300

    ./,lk

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,6 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-
 DEBUG = bool(os.getenv("DEBUG"))
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
